390665a0e094126574f8c04de59ba507b139a67a
lamen3737 <61162799+lamen3737@users.noreply.github.com>
2020-02-17 23:27:34 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const config = {
-    "ownerID": "446048541488578569", //kendi IDınızı yazınız
-    "admins": ["446048541488578569"],
-    "support": ["446048541488578569"],
-    "token": "NDg1MTEwODUyNzM5OTIzOTY5.XWXoOA.R7fx41HJF9y-JMHWJvxNndZOPzk", //botunuzun tokenini yazınız
+    "ownerID": "259986246892388354", //kendi IDınızı yazınız
+    "admins": ["259986246892388354"],
+    "support": ["259986246892388354"],
+    "token": "Njc5MDQwNTUwMDY1OTMwMjUw.XkrvIQ.x_h1XNgmCMt11JA6m9XIzX2fHY8", //botunuzun tokenini yazınız
     "dashboard" : {
-      "oauthSecret": "lNYupwJAzgSjqLSgnf8z5zZITsalw2K4", //botunuzun secretini yazınız
-      "callbackURL": `https://anloxbot-gecilcek.glitch.me/callback`, //site URLnizi yazınız /callback kısmını silmeyiniz!
+      "oauthSecret": "Njc5MDQwNTUwMDY1OTMwMjUw.XkrvIQ.x_h1XNgmCMt11JA6m9XIzX2fHY8", //botunuzun secretini yazınız
+      "callbackURL": `https://puffy-shaker-kkjln5ipua.glitch.me/callback`, //site URLnizi yazınız /callback kısmını silmeyiniz!
       "sessionSecret": "xyzxyz", //kalsın
-      "domain": "https://anloxbot-gecilcek.glitch.me/" //site URLnizi yazınız!
+      "domain": "https://puffy-shaker-kkjln5ipua.glitch.me/" //site URLnizi yazınız!
     }
   };
   
