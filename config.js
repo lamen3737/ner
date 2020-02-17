2198242d9ec57705950f29535ba34d09d6205fda
lamen3737 <61162799+lamen3737@users.noreply.github.com>
2020-02-17 23:32:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     "ownerID": "259986246892388354", //kendi IDınızı yazınız
     "admins": ["259986246892388354"],
     "support": ["259986246892388354"],
-    "token": "Njc5MDQwNTUwMDY1OTMwMjUw.XkrvIQ.x_h1XNgmCMt11JA6m9XIzX2fHY8", //botunuzun tokenini yazınız
+    "token": "lNYupwJAzgSjqLSgnf8z5zZITsalw2K4", //botunuzun tokenini yazınız
     "dashboard" : {
       "oauthSecret": "Njc5MDQwNTUwMDY1OTMwMjUw.XkrvIQ.x_h1XNgmCMt11JA6m9XIzX2fHY8", //botunuzun secretini yazınız
       "callbackURL": `https://anloxbot-gecilcek.glitch.me/callback`, //site URLnizi yazınız /callback kısmını silmeyiniz!
